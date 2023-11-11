5db427762f2aba8ad4d6035343914b0e29de84c5
Andrey Shilkov <ashilkov2000@mail.ru>
2023-11-11 11:51:36 +0500

    fixed configs

--- assets/tsn_family/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_family.py
+++ assets/tsn_family/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_family.py
@@ -1,151 +1,257 @@
-_base_ = [
-    "..\..\..\configs\_base_\schedules\sgd_100e.py",
-    "..\..\..\configs\_base_\default_runtime.py",
-]
-
-
-# dataset settings
-dataset_type = "VideoDataset"
-data_root = "E:\ITMO\CVProject\data\slovo\\train"
-data_root_val = "E:\ITMO\CVProject\data\slovo\\test"
-ann_file_train = "E:\ITMO\CVProject\mmaction_datasets\\family\\train_ann.txt"
-ann_file_val = "E:\ITMO\CVProject\mmaction_datasets\\family\\val_ann.txt"
-
-file_client_args = dict(io_backend="disk")
-
-model = dict(
-    type="Recognizer2D",
-    backbone=dict(
-        type="ResNet",
-        pretrained="https://download.pytorch.org/models/resnet50-11ad3fa6.pth",
-        depth=50,
-        norm_eval=False,
-    ),
-    cls_head=dict(
-        type="TSNHead",
-        num_classes=8,
-        in_channels=2048,
-        spatial_type="avg",
-        consensus=dict(type="AvgConsensus", dim=1),
-        dropout_ratio=0.4,
-        init_std=0.01,
-        average_clips="prob",
-    ),
-    data_preprocessor=dict(
-        type="ActionDataPreprocessor",
-        mean=[123.675, 116.28, 103.53],
-        std=[58.395, 57.12, 57.375],
-        format_shape="NCHW",
-    ),
-    train_cfg=None,
-    test_cfg=None,
-)
-
-train_pipeline = [
-    dict(type="DecordInit", **file_client_args),
-    dict(type="SampleFrames", clip_len=1, frame_interval=1, num_clips=3),
-    dict(type="DecordDecode"),
-    dict(type="Resize", scale=(-1, 256)),
-    dict(
-        type="MultiScaleCrop",
-        input_size=224,
-        scales=(1, 0.875, 0.75, 0.66),
-        random_crop=False,
-        max_wh_scale_gap=1,
-    ),
-    dict(type="Resize", scale=(224, 224), keep_ratio=False),
-    # dict(type="Flip", flip_ratio=0.5),
-    dict(type="FormatShape", input_format="NCHW"),
-    dict(type="PackActionInputs"),
-]
-val_pipeline = [
-    dict(type="DecordInit", **file_client_args),
-    dict(
-        type="SampleFrames", clip_len=1, frame_interval=1, num_clips=3, test_mode=True
-    ),
-    dict(type="DecordDecode"),
-    dict(type="Resize", scale=(-1, 256)),
-    dict(type="CenterCrop", crop_size=224),
-    dict(type="FormatShape", input_format="NCHW"),
-    dict(type="PackActionInputs"),
-]
-test_pipeline = [
-    dict(type="DecordInit", **file_client_args),
-    dict(
-        type="SampleFrames", clip_len=1, frame_interval=1, num_clips=25, test_mode=True
-    ),
-    dict(type="DecordDecode"),
-    dict(type="Resize", scale=(-1, 256)),
-    dict(type="TenCrop", crop_size=224),
-    dict(type="FormatShape", input_format="NCHW"),
-    dict(type="PackActionInputs"),
-]
-
-train_dataloader = dict(
-    batch_size=8,
-    num_workers=0,
-    persistent_workers=False,
-    sampler=dict(type="DefaultSampler", shuffle=True),
-    dataset=dict(
-        type=dataset_type,
-        ann_file=ann_file_train,
-        data_prefix=dict(video=data_root),
-        pipeline=train_pipeline,
-    ),
-)
-val_dataloader = dict(
-    batch_size=8,
-    num_workers=0,
-    persistent_workers=False,
-    sampler=dict(type="DefaultSampler", shuffle=False),
-    dataset=dict(
-        type=dataset_type,
-        ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
-        pipeline=val_pipeline,
-        test_mode=True,
-    ),
-)
-test_dataloader = dict(
-    batch_size=8,
-    num_workers=0,
-    persistent_workers=True,
-    sampler=dict(type="DefaultSampler", shuffle=False),
-    dataset=dict(
-        type=dataset_type,
-        ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
-        pipeline=test_pipeline,
-        test_mode=True,
-    ),
-)
-
-val_evaluator = dict(
-    type="F1Metric",
-    label2id_mapping_path="E:\ITMO\CVProject\mmaction_datasets\\family\\label2id_mapping.json",
-)
-test_evaluator = val_evaluator
-
+ann_file_train = 'E:\\ITMO\\CVProject\\mmaction_datasets\\family\\train_ann.txt'
+ann_file_val = 'E:\\ITMO\\CVProject\\mmaction_datasets\\family\\val_ann.txt'
+auto_scale_lr = dict(base_batch_size=256, enable=False)
+data_root = 'E:\\ITMO\\CVProject\\data\\slovo\\train'
+data_root_val = 'E:\\ITMO\\CVProject\\data\\slovo\\test'
+dataset_type = 'VideoDataset'
 default_hooks = dict(
     checkpoint=dict(
-        type="CheckpointHook",
         interval=1,
-        save_best="weighted_avg_f1_score",
-        rule="greater",
-    ),
-)
-
-vis_backends = [
-    # dict(type="LocalVisBackend"),
+        rule='greater',
+        save_best='weighted_avg_f1_score',
+        type='CheckpointHook'),
+    logger=dict(ignore_last=False, interval=20, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    runtime_info=dict(type='RuntimeInfoHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    sync_buffers=dict(type='SyncBuffersHook'),
+    timer=dict(type='IterTimerHook'))
+default_scope = 'mmaction'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+file_client_args = dict(io_backend='disk')
+launcher = 'none'
+load_from = None
+log_level = 'INFO'
+log_processor = dict(by_epoch=True, type='LogProcessor', window_size=20)
+model = dict(
+    backbone=dict(
+        depth=50,
+        norm_eval=False,
+        pretrained='https://download.pytorch.org/models/resnet50-11ad3fa6.pth',
+        type='ResNet'),
+    cls_head=dict(
+        average_clips='prob',
+        consensus=dict(dim=1, type='AvgConsensus'),
+        dropout_ratio=0.4,
+        in_channels=2048,
+        init_std=0.01,
+        num_classes=8,
+        spatial_type='avg',
+        type='TSNHead'),
+    data_preprocessor=dict(
+        format_shape='NCHW',
+        mean=[
+            123.675,
+            116.28,
+            103.53,
+        ],
+        std=[
+            58.395,
+            57.12,
+            57.375,
+        ],
+        type='ActionDataPreprocessor'),
+    test_cfg=None,
+    train_cfg=None,
+    type='Recognizer2D')
+optim_wrapper = dict(
+    clip_grad=dict(max_norm=40, norm_type=2),
+    optimizer=dict(lr=0.01, momentum=0.9, type='SGD', weight_decay=0.0001))
+param_scheduler = [
     dict(
-        type="ExtendedClearMLVisBackend",
-        init_kwargs=dict(project_name="CVProject", task_name="train_family_package"),
-    ),
+        begin=0,
+        by_epoch=True,
+        end=100,
+        gamma=0.1,
+        milestones=[
+            40,
+            80,
+        ],
+        type='MultiStepLR'),
 ]
-visualizer = dict(type="ClearMLExtendedVisualizer", vis_backends=vis_backends)
-
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (32 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=256)
+randomness = dict(deterministic=False, diff_rank_seed=False, seed=None)
+resume = False
+test_cfg = dict(type='TestLoop')
+test_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        ann_file='E:\\ITMO\\CVProject\\mmaction_datasets\\family\\val_ann.txt',
+        data_prefix=dict(video='E:\\ITMO\\CVProject\\data\\slovo\\test'),
+        pipeline=[
+            dict(io_backend='disk', type='DecordInit'),
+            dict(
+                clip_len=1,
+                frame_interval=1,
+                num_clips=25,
+                test_mode=True,
+                type='SampleFrames'),
+            dict(type='DecordDecode'),
+            dict(scale=(
+                -1,
+                256,
+            ), type='Resize'),
+            dict(crop_size=224, type='TenCrop'),
+            dict(input_format='NCHW', type='FormatShape'),
+            dict(type='PackActionInputs'),
+        ],
+        test_mode=True,
+        type='VideoDataset'),
+    num_workers=0,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = dict(
+    label2id_mapping_path=
+    'E:\\ITMO\\CVProject\\mmaction_datasets\\family\\label2id_mapping.json',
+    type='F1Metric')
+test_pipeline = [
+    dict(io_backend='disk', type='DecordInit'),
+    dict(
+        clip_len=1,
+        frame_interval=1,
+        num_clips=25,
+        test_mode=True,
+        type='SampleFrames'),
+    dict(type='DecordDecode'),
+    dict(scale=(
+        -1,
+        256,
+    ), type='Resize'),
+    dict(crop_size=224, type='TenCrop'),
+    dict(input_format='NCHW', type='FormatShape'),
+    dict(type='PackActionInputs'),
+]
+train_cfg = dict(
+    max_epochs=100, type='EpochBasedTrainLoop', val_begin=1, val_interval=1)
+train_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        ann_file=
+        'E:\\ITMO\\CVProject\\mmaction_datasets\\family\\train_ann.txt',
+        data_prefix=dict(video='E:\\ITMO\\CVProject\\data\\slovo\\train'),
+        pipeline=[
+            dict(io_backend='disk', type='DecordInit'),
+            dict(
+                clip_len=1, frame_interval=1, num_clips=3,
+                type='SampleFrames'),
+            dict(type='DecordDecode'),
+            dict(scale=(
+                -1,
+                256,
+            ), type='Resize'),
+            dict(
+                input_size=224,
+                max_wh_scale_gap=1,
+                random_crop=False,
+                scales=(
+                    1,
+                    0.875,
+                    0.75,
+                    0.66,
+                ),
+                type='MultiScaleCrop'),
+            dict(keep_ratio=False, scale=(
+                224,
+                224,
+            ), type='Resize'),
+            dict(flip_ratio=0.5, type='Flip'),
+            dict(input_format='NCHW', type='FormatShape'),
+            dict(type='PackActionInputs'),
+        ],
+        type='VideoDataset'),
+    num_workers=0,
+    persistent_workers=False,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(io_backend='disk', type='DecordInit'),
+    dict(clip_len=1, frame_interval=1, num_clips=3, type='SampleFrames'),
+    dict(type='DecordDecode'),
+    dict(scale=(
+        -1,
+        256,
+    ), type='Resize'),
+    dict(
+        input_size=224,
+        max_wh_scale_gap=1,
+        random_crop=False,
+        scales=(
+            1,
+            0.875,
+            0.75,
+            0.66,
+        ),
+        type='MultiScaleCrop'),
+    dict(keep_ratio=False, scale=(
+        224,
+        224,
+    ), type='Resize'),
+    dict(flip_ratio=0.5, type='Flip'),
+    dict(input_format='NCHW', type='FormatShape'),
+    dict(type='PackActionInputs'),
+]
+val_cfg = dict(type='ValLoop')
+val_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        ann_file='E:\\ITMO\\CVProject\\mmaction_datasets\\family\\val_ann.txt',
+        data_prefix=dict(video='E:\\ITMO\\CVProject\\data\\slovo\\test'),
+        pipeline=[
+            dict(io_backend='disk', type='DecordInit'),
+            dict(
+                clip_len=1,
+                frame_interval=1,
+                num_clips=3,
+                test_mode=True,
+                type='SampleFrames'),
+            dict(type='DecordDecode'),
+            dict(scale=(
+                -1,
+                256,
+            ), type='Resize'),
+            dict(crop_size=224, type='CenterCrop'),
+            dict(input_format='NCHW', type='FormatShape'),
+            dict(type='PackActionInputs'),
+        ],
+        test_mode=True,
+        type='VideoDataset'),
+    num_workers=0,
+    persistent_workers=False,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = dict(
+    label2id_mapping_path=
+    'E:\\ITMO\\CVProject\\mmaction_datasets\\family\\label2id_mapping.json',
+    type='F1Metric')
+val_pipeline = [
+    dict(io_backend='disk', type='DecordInit'),
+    dict(
+        clip_len=1,
+        frame_interval=1,
+        num_clips=3,
+        test_mode=True,
+        type='SampleFrames'),
+    dict(type='DecordDecode'),
+    dict(scale=(
+        -1,
+        256,
+    ), type='Resize'),
+    dict(crop_size=224, type='CenterCrop'),
+    dict(input_format='NCHW', type='FormatShape'),
+    dict(type='PackActionInputs'),
+]
+vis_backends = [
+    dict(
+        init_kwargs=dict(
+            project_name='CVProject', task_name='train_family_package'),
+        type='ExtendedClearMLVisBackend'),
+]
+visualizer = dict(
+    type='ClearMLExtendedVisualizer',
+    vis_backends=[
+        dict(
+            init_kwargs=dict(
+                project_name='CVProject', task_name='train_family_package'),
+            type='ExtendedClearMLVisBackend'),
+    ])
+work_dir = './work_dirs\\tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_family'
